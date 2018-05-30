25baa4388b7ff26b195980c0cb89c797fa5ef25f
Miles Lucas <mdlucas@nrao.edu>
2018-05-30 00:25:35 -0600

    added a plotting method, need to write unit tests

--- scripts/compare.py
+++ scripts/compare.py
@@ -2,11 +2,9 @@
 # Miles Lucas - mdlucas@nrao.edu
 
 import numpy as np
-# from matplotlib import rc
-# import matplotlib.pyplot as plt
 import argparse
 
-def compare(image_a, image_b, regrid=False):
+def compare(image_a, image_b, regrid=False, plot=True):
 	'''
 	Compare the given images
 	'''
@@ -14,6 +12,14 @@ def compare(image_a, image_b, regrid=False):
 		image_b = regrid(image_a, image_b)
 	smap_a, amps_a = get_arrays(image_a)
 	smap_b, amps_b = get_arrays(image_b)
+
+	r_a, pow_a = get_psd(smap_a, amps_a)
+	r_b, pow_b = get_psd(smap_b, amps_b)
+
+	if plot:
+		camparison_plot(r_a, pow_a, r_b, pow_b)
+
+	return r_a, pow_a, r_b, pow_b
 
 	
 
@@ -75,18 +81,53 @@ def get_arrays(image_path):
 	return smap, amps
 
 
-def arr_transform(smap, amps):
+def get_psd(smap, amps):
 	'''
 	Turn the 2D data into 1D data by taking the distance from the origin
 	'''
+	# Get the frequencies
+	freq_x = np.fft.fftfreq(smap['n_x'], smap['d_x'])
+	freq_y = np.fft.fftfreq(smap['n_y'], smap['d_y'])
+
+	fft = np.fft.fft2(amps)
+
 	dist = []
 	power = []
 	for i, x in enumerate(freq_x):
 		for j, y in enumerate(freq_y):
 			dist.append(np.linalg.norm((x, y)))
-			power.append(amps[i, j])
+			power.append(fft[i, j])
 
 	return np.array(dist), np.array(power)
+
+def comparison_plot(r_a, pow_a, r_b, pow_b):
+	'''
+	Plots comparison of the two power spectrum
+	'''
+	import matplotlib.pyplot as plt
+	from matplotlib import rcParams
+
+	# Style
+	plt.style.use('seaborn')
+	style = {
+		'figure.figsize': (12, 8),
+		'axes.labelsize': 14,
+		'axes.titlesize': 16,
+		'legend.fontsize': 14,
+	}
+	rcParams.update(style)
+
+	# Plot
+	plt.figure()
+	plt.semilogy(r_a, pow_a, label='Image A')
+	plt.semilogy(r_b, pow_b, label='Image B')
+	plt.title('Comparison of PSD')
+	plt.xlabel(r'f')
+	plt.ylabel(r'Power (unnormalized)')
+	plt.legend(loc='best')
+	plt.show()
+
+
 
 
 if __name__ == '__main__':
@@ -94,5 +135,6 @@ if __name__ == '__main__':
 	parser.add_argument('image_a', help='path to the first image')
 	parser.add_argument('image_b', help='path to the second image')
 	parser.add_argument('-r', '--regrid', action='store_true', help="regrids image_b to image_a's coordinates")
+	parser.add_argument('-n', '--no-plot', dest='plot', action='store_false')
 	args = parser.parse_args()
-	compare(args.image_a, args.image_b, args.regrid)
+	compare(args.image_a, args.image_b, args.regrid, args.plot)
