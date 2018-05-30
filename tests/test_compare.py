470b4fa24bdb50f06161f8a8a06d9d8634584855
Miles Lucas <mdlucas@nrao.edu>
2018-05-29 19:03:20 -0600

    added unit test for array transforms

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -1,5 +1,6 @@
 import unittest
 from scripts import compare
+import numpy as np
 
 class TestCompare(unittest.Testcase):
 
@@ -9,7 +10,16 @@ class TestCompare(unittest.Testcase):
 	def test_get_arr(self):
 		data_path = 'data/orion.gbt.im'
 		arr = compare.get_arr(data_path)
-		self.assertEqual(arr.shape, (300,300))	
+		self.assertEqual(arr.shape, (300,300))
+
+	def test_arr_transform(self):
+		fx = np.linspace(1, 1000, 101)
+		fy = np.linspace(1, 1000, 101)
+		amps = np.random.normal(loc=100, scale=10, size=(101, 101))
+		r, a = compare.arr_transform(fx, fy, amps)
+		self.assertEqual(len(a), amps.shape[0]*amps.shape[1])
+		self.assertFalse(min(r) < 0)
+		self.assertEqual(len(r), len(a))
 
 
 if __name__=='__main__':
