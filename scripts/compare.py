078a6fa5dad06e5196a5dc17df1cf2c36517f5cd
Miles Lucas <mdlucas@nrao.edu>
2018-05-29 18:39:11 -0600

    edited some of get_arrays, added a helper for square distance

--- scripts/compare.py
+++ scripts/compare.py
@@ -10,6 +10,11 @@ def compare(image_a, image_b):
 	'''
 	Compare the given images
 	'''
+	waves_a, amps_a = get_arrays(image_a)
+	waves_b, amps_b = get_arrays(image_b)
+
+	
+
 	
 
 def get_arrays(image_path):
@@ -23,15 +28,39 @@ def get_arrays(image_path):
 	
 	Returns
 	-------
-	im_arr: ndarray
-		An array of shape (N,M) where N and M are the resolution of the image	     '''
-	
-	tb.open(image_path)
-	raw_data = tb.getcol('map')
-	# The original shape may be something like (300,300,1,1,1) so we squeeze
-	im_arr = np.squeeze(raw_data)
-	return im_arr
-	
+	waves: ndarray
+		A 2 dimensional array of the wavelengths of the fft
+	amps: ndarray
+		A 2 dimensional array of the amplitudes of the fft
+	'''
+	ia.open(image_path)
+	ia.fft(real='r.im', amps='a.im')
+	ia.close()
+
+	tb.open('r.im')
+	waves = np.squeeze(tb.getcol('map'))
+	tb.close()
+
+	tb.open('a.im')
+	amps = np.squeeze(tb.getcol('map'))
+	tb.close()
+
+	ia.removefile('r.im')
+	ia.removefile('a.im')
+
+	return waves, amps
+
+
+def norm_transform(waves, amps):
+	'''
+	Turn the 2D data into 1D data by taking the distance from the origin
+	'''
+	dist = []
+	power = []
+	for i, row in enumerate(waves):
+		for j, col in enumerate(row):
+			r = np.linalg.norm()
+			
 
 
 if __name__ == '__main__':
